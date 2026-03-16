763948715ffe215e9ad8397633e0124002664082
Gabi Kis <gabikissv@yahoo.com>
2026-03-16 16:41:55 +0200

    Cleanup

--- src/test/java/starter/pageobjects/ProductPage.java
+++ src/test/java/starter/pageobjects/ProductPage.java
@@ -5,4 +5,5 @@ import org.openqa.selenium.By;
 
 public class ProductPage extends PageObject {
     public static final By PRODUCT_NAME = By.cssSelector(".productname span");
+    public static final By PRODUCT_PRICE = By.cssSelector(".productfilneprice");
 }
